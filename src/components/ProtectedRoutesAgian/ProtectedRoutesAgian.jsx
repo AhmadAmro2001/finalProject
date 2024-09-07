27a275859303405a1282e06701536612ef3517f9
AhmadAmro2001 <gamerteacher12@gmail.com>
2024-09-07 20:24:35 +0200

    vercelAgain

--- src/components/ProtectedRoutesAgian/ProtectedRoutesAgian.jsx
+++ src/components/ProtectedRoutesAgian/ProtectedRoutesAgian.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import styles from './ProtectedRoutesAgian.module.css'
 import { Navigate } from 'react-router-dom'
-export default function ProtectedRoutesAgian({children}) {
+export default function ProtectedRoutesAgian(props) {
     if(!localStorage.getItem("token")){
         return(
             <>
-            {children}
+            {props.children}
             </>
         ) 
     }else{
